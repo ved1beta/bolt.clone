f920244da9692f6378b8532ee6a1b789a70becff
ved1beta <vedantthote2019@gmail.com>
2024-12-08 08:54:03 +0530

    streaming func added engg prompt

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,2 +1,17 @@
 require("dotenv").config();
 console.log(process.env.GEMINI_API_KEY)
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("AIzaSyCMJN_HqVPaHUEKeR_FfKxNwXhHcKXf-oE");
+
+async function main() {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const prompt = "hoo to write my first reach app";
+    const result = await model.generateContentStream(prompt);
+    for await (const chunk of result.stream) {
+        process.stdout.write(chunk.text());
+    }
+}
+main();
+
